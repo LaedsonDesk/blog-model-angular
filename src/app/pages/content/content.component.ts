3c01aca7bb756fa21675e1a6521fe3067a20b045
Laedson <laedson2535@gmail.com>
2025-03-27 09:30:47 -0300

    Corrigindo erro do Vercel

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -4,33 +4,35 @@ import { dataFake } from '../data/dataFake';
 
 @Component({
   selector: 'app-content',
-  imports: [RouterModule],
   templateUrl: './content.component.html',
+  imports: [RouterModule],
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  private id: string | null = "";
 
-photoCover:string= ""
-contentTitle:string= ""
-contentDescription:string= ""
-private id:string | null = ""
+  constructor(private route: ActivatedRoute) {}
 
-  constructor(
-    private route:ActivatedRoute
-  ){ }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get("id");
 
-ngOnInit(): void {
-  this.route.paramMap.subscribe( value => 
-    this.id = value.get("id")
-  )
-  this.setValuesToComponent(this.id)
+      if (this.id) {  // Só chama a função se o ID for válido
+        this.setValuesToComponent(this.id);
+      }
+    });
   }
-  setValuesToComponent(id:string | null){
-    const result = dataFake.filter(article => article.id == id)[0]
 
-    this.contentTitle = result.title
-    this.contentDescription = result.description
-    this.photoCover = result.photoCover
+  setValuesToComponent(id: string): void {
+    const result = dataFake.find(article => article.id === id);
 
+    if (result) {
+      this.contentTitle = result.title;
+      this.contentDescription = result.description;
+      this.photoCover = result.photoCover;
     }
   }
+}
